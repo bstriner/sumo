f8c7cddaa3176a3121d2dff8cdfcaacf1e9a566f
Mahmoud Nawar <Mahmoud.Nawar@feng.bu.edu.eg>
2017-02-09 22:19:11 +0200

    add more functionality

--- sumo/src/libsumo/vehicle.cpp
+++ sumo/src/libsumo/vehicle.cpp
@@ -82,6 +82,21 @@ libsumo_vehicle_speed(const char* id)
 	return visible ? v->getSpeed() : INVALID_DOUBLE_VALUE;
 }
 
+LIBSUMO_DLL_EXPORTED double
+libsumo_vehicle_waitingtime(const char* id)
+{
+	SUMOVehicle* sumoVehicle = libsumo_net->getVehicleControl().getVehicle(id);
+	if (sumoVehicle == 0) {
+		throw ProcessError("Unknown vehicle");
+	}
+	MSVehicle* v = dynamic_cast<MSVehicle*>(sumoVehicle);
+	if (v == 0) {
+		throw ProcessError( "Vehicle is not a micro-simulation vehicle");
+	}
+
+	return v->getWaitingSeconds();
+}
+
 LIBSUMO_DLL_EXPORTED void
 libsumo_vehicle_positions(double* positions)
 {
